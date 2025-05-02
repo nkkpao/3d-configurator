694b0ed345b5eec10f1fa1ec6ef4ace9fe8ce811
nkkpao <99089421+nkkpao@users.noreply.github.com>
2025-05-02 17:49:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/app/main.tsx'),
-    },
-  }
 });
